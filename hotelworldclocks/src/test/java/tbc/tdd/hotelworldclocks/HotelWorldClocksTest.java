0a15130bfa09a4e6e4e44f1f8e964760f5341de1
Ben Wu <wubin28@gmail.com>
2014-05-13 22:59:28 +0800

    Wrote intention code in test the_time_of_clock_London_and_NewYork_should_be_1_and_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time().

--- hotelworldclocks/src/test/java/tbc/tdd/hotelworldclocks/HotelWorldClocksTest.java
+++ hotelworldclocks/src/test/java/tbc/tdd/hotelworldclocks/HotelWorldClocksTest.java
@@ -39,6 +39,17 @@ public class HotelWorldClocksTest {
     // TODO-working-on: Set time to multiple city clocks
     @Test
     public void the_time_of_clock_London_and_NewYork_should_be_1_and_20_respectively_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+        CityClock londonClock = new CityClock(0);
+        CityClock newYorkClock = new CityClock(-5);
+        HotelWorldClockSystem hotelWorldClockSystem = new HotelWorldClockSystem();
+        hotelWorldClockSystem.attach(londonClock);
+        hotelWorldClockSystem.attach(newYorkClock);
+        PhoneClock phoneClock = new PhoneClock(8);
+
+        // Act
+        phoneClock.setHotelWorldClockSystem(hotelWorldClockSystem);
+        phoneClock.setTime(9);
 
         // Assert
         assertEquals(1, londonClock.getTime());
