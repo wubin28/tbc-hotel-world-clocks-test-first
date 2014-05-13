afa6a1ef8c6c258b27d8a98f7b1e37ef2a12ec25
Ben Wu <wubin28@gmail.com>
2014-05-13 21:38:51 +0800

    Created field cityClock and assigned a value to it in class PhoneClock.

--- hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/PhoneClock.java
+++ hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/PhoneClock.java
@@ -4,12 +4,14 @@ package tbc.tdd.hotelworldclocks;
  * Created by benwu on 14-5-13.
  */
 public class PhoneClock {
+    private CityClock cityClock;
+
     public PhoneClock(int utcOffset) {
 
     }
 
     public void setCityClock(CityClock cityClock) {
-
+        this.cityClock = cityClock;
     }
 
     public void setTime(int time) {
