3ac87395bbc31643fcde706f8076e1783c8c711f
Ben Wu <wubin28@gmail.com>
2014-05-13 22:37:38 +0800

    Updated method CityClock.getTime() to make the hour within 24.

--- hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/CityClock.java
+++ hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/CityClock.java
@@ -13,7 +13,7 @@ public class CityClock {
     }
 
     public int getTime() {
-        return this.utcOffset + this.utcZeroTime;
+        return (this.utcOffset + this.utcZeroTime + 24) % 24;
     }
 
     public void setUtcZeroTime(int utcZeroTime) {
