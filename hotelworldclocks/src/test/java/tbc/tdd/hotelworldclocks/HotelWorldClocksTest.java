7c95d4acfc1515380571bb7acec7d4170cb5037b
Ben Wu <wubin28@gmail.com>
2014-05-13 20:35:13 +0800

    Instantiated a CityClock and a PhoneClock with UTC offset in the test.

--- hotelworldclocks/src/test/java/tbc/tdd/hotelworldclocks/HotelWorldClocksTest.java
+++ hotelworldclocks/src/test/java/tbc/tdd/hotelworldclocks/HotelWorldClocksTest.java
@@ -11,6 +11,8 @@ public class HotelWorldClocksTest {
     @Test
     public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
         // Arrange
+        CityClock londonClock = new CityClock(0);
+        PhoneClock phoneClock = new PhoneClock(8);
 
         // Act
         phoneClock.setCityClock(londonClock);
