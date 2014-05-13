710209520b5347c8768e2f8f4c82200dbec57123
Ben Wu <wubin28@gmail.com>
2014-05-13 21:22:52 +0800

    Created field utcOffset in class CityClock and assigned a value to it in the constructor.

--- hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/CityClock.java
+++ hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/CityClock.java
@@ -4,9 +4,11 @@ package tbc.tdd.hotelworldclocks;
  * Created by benwu on 14-5-13.
  */
 public class CityClock {
+    private int utcOffset;
+
     // TODO: The constructors of CityClock and PhoneClock are duplicated
     public CityClock(int utcOffset) {
-
+        this.utcOffset = utcOffset;
     }
 
     public int getTime() {
