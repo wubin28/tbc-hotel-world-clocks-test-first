f8f0f5c481b6f33f9d5816f8f03edb86355beccb
Ben Wu <wubin28@gmail.com>
2014-05-13 22:31:36 +0800

    Added test the_time_of_clock_NewYork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() and got a negative hour in the actual result.

--- hotelworldclocks/src/test/java/tbc/tdd/hotelworldclocks/HotelWorldClocksTest.java
+++ hotelworldclocks/src/test/java/tbc/tdd/hotelworldclocks/HotelWorldClocksTest.java
@@ -23,5 +23,19 @@ public class HotelWorldClocksTest {
     }
 
     // TODO-working-on: Negative hour
+    @Test
+    public void the_time_of_clock_NewYork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+        CityClock newYorkClock = new CityClock(-5);
+        PhoneClock phoneClock = new PhoneClock(8);
+
+        // Act
+        phoneClock.setCityClock(newYorkClock);
+        phoneClock.setTime(9);
+
+        // Assert
+        assertEquals(20, newYorkClock.getTime());
+    }
+
     // TODO: Set time to multiple city clocks
 }
