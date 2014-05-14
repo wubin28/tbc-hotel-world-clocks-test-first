a91b99a415fc5c72c869f67d59a4b0e759227fef
Ben Wu <wubin28@gmail.com>
2014-05-14 09:00:07 +0800

    Updated method HotelWorldClockSystem.attach() to attach a CityClock.

--- hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/HotelWorldClockSystem.java
+++ hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/HotelWorldClockSystem.java
@@ -9,7 +9,7 @@ public class HotelWorldClockSystem {
     private ArrayList<CityClock> cityClocks = new ArrayList<CityClock>();
 
     public void attach(CityClock cityClock) {
-
+        this.cityClocks.add(cityClock);
     }
 
     public ArrayList<CityClock> getClocks() {
