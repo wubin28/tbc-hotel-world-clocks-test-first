90a8ed122696aca7677854c5d0d9a86db306f892
Ben Wu <wubin28@gmail.com>
2014-05-14 08:55:38 +0800

    Created method HotelWorldClockSystem.getClocks() to return a clock list stored in this class.

--- hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/HotelWorldClockSystem.java
+++ hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/HotelWorldClockSystem.java
@@ -1,10 +1,16 @@
 package tbc.tdd.hotelworldclocks;
 
+import java.util.ArrayList;
+
 /**
  * Created by benwu on 14-5-13.
  */
 public class HotelWorldClockSystem {
     public void attach(CityClock cityClock) {
 
+    }
+
+    public ArrayList<CityClock> getClocks() {
+        return this.cityClocks;
     }
 }
