3c696086af978fae0cf534dd08d83eed757862ee
Ben Wu <wubin28@gmail.com>
2014-05-15 22:28:59 +0800

    Created field time in class PhoneClock and assigned value to it in method setTime().

--- hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/PhoneClock.java
+++ hotelworldclocks/src/main/java/tbc/tdd/hotelworldclocks/PhoneClock.java
@@ -5,12 +5,14 @@ package tbc.tdd.hotelworldclocks;
  */
 public class PhoneClock extends Clock {
     private HotelWorldClockSystem hotelWorldClockSystem;
+    private int time;
 
     public PhoneClock(int utcOffset) {
             super.utcOffset = utcOffset;
     }
 
     public void setTime(int time) {
+        this.time = time;
         if (this.hotelWorldClockSystem == null) return;
         for (CityClock cityClock : this.hotelWorldClockSystem.getClocks()) {
             cityClock.setUtcZeroTime(time - super.utcOffset);
